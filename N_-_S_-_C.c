461b5ab229720ad0fdec08bd48ed8f9c46496bbf
Santhoshi003 <147791940+Santhoshi003@users.noreply.github.com>
2023-11-23 10:41:24 +0530

    #include<stdio.h> int main() {     int i,a,b,s,c;     scanf("%d%d",&a,&b);     for(i=a+1;i<=b-1;i++)     {         s=i*i;         c=i*i*i;     printf("%d %d %d ",i,s,c);} }

---
+++ N_-_S_-_C.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,a,b,s,c;
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<=b-1;i++)
+    {
+        s=i*i;
+        c=i*i*i;
+    printf("%d %d %d
+",i,s,c);}
+}
